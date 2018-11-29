a2af152bc1e0c88de5dff41639c8f9d212e9f0bd
kevinhuynh <khuynh23@uw.edu>
2018-11-29 10:55:50 -0800

    moved api

--- day-17/src/index.js
+++ day-17/src/index.js
@@ -5,15 +5,7 @@ import App from './App';
 import * as serviceWorker from './serviceWorker';
 import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
 import firebase from 'firebase/app';
-
-const config = {
-        apiKey: "AIzaSyB0y--IC7yo2iWVTHuuOWtLVSWNgZoLm9E",
-        authDomain: "info-auth-e7f30.firebaseapp.com",
-        databaseURL: "https://info-auth-e7f30.firebaseio.com",
-        projectId: "info-auth-e7f30",
-        storageBucket: "info-auth-e7f30.appspot.com",
-        messagingSenderId: "320548498580"
-      };
+import config from './Config.js';
 
 firebase.initializeApp(config);
 ReactDOM.render(<App />, document.getElementById('root'));
